410d30e8f821166f95271d886c18d08a9f826bd8
Christopher Lovell <clovell@student.42adel.org.au>
2023-09-25 19:45:52 +0930

    Add ft_strdupctx

--- include/libft_extra.h
+++ include/libft_extra.h
@@ -6,18 +6,22 @@
 /*   By: clovell <clovell@student.42adel.org.au>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 23:03:48 by clovell           #+#    #+#             */
-/*   Updated: 2023/09/04 21:48:05 by clovell          ###   ########.fr       */
+/*   Updated: 2023/09/20 16:55:28 by clovell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdbool.h>
 /* String Duplicate Until
  * Creates a duplicated string from that start of 'src'
  * Until a character from 'until' is found.
  */
+typedef int	(*t_strdupctxfn)(char*, int, bool, void*);
+
 char *ft_strdupu(char *src, char *until);
 char *ft_strdupct(char *src, int (*check)(int c));
 char *ft_strdupi(char *src, int (*check)(char *src, int index));
+char *ft_strdupctx(char *src, void *ctx, t_strdupctxfn check);
 
 int	ft_isspace(int c);
 int	ft_isalnum(int c);
--- src/utils/ft_strdup_extra.c
+++ src/utils/ft_strdup_extra.c
@@ -6,12 +6,13 @@
 /*   By: clovell <clovell@student.42adel.org.au>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 20:15:32 by clovell           #+#    #+#             */
-/*   Updated: 2023/09/07 19:44:11 by clovell          ###   ########.fr       */
+/*   Updated: 2023/09/20 16:55:49 by clovell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
+#include "libft_extra.h"
 
 /* Creates a duplicated string from that start of 'src'
  * Until a character from 'until' is found.
@@ -104,6 +105,38 @@ char *ft_strdupi(char *src, int (*check)(char *src, int index))
 	while (src[i])
 	{
 		state = check(src, i);
+		if (state == 2)
+			break ;
+		else if (state == 1)
+			ret[j++] = src[i];
+		i++;
+	}
+	ret[j] = '\0';
+	return (ret);
+}
+
+// TODO: Info
+char *ft_strdupctx(char *src, void *ctx, t_strdupctxfn check)
+{
+	int	i;
+	int	j;
+	char *ret;
+	int state;
+
+	i = 0;
+	if (src == NULL)
+		return (NULL);
+	while (src[i] && check(src, i, true, ctx) != 2)
+		i++;
+	ret = malloc(sizeof(char) * (i + 1));
+	if (ret == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	state = 0;
+	while (src[i])
+	{
+		state = check(src, i, false, ctx);
 		if (state == 2)
 			break ;
 		else if (state == 1)
